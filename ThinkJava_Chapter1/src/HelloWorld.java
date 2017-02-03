fc8e5512b2655d3f88d42f60eb2cb5d12e3e302e
soumya <soumya3587@gmail.com>
2017-02-03 16:12:13 -0600

    Very First Commit. Commiting chapter 1 solutions

---
+++ ThinkJava_Chapter1/src/HelloWorld.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+/**
+ * @author soumya
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("Hello, world.");
+		 
+		 //I am going to try printing a second message. 
+		 
+		 System.out.println("How are you?");
+
+	}
+
+}
